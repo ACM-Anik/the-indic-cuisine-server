0bfd2b4fdcd21afe64b01a697372397db609407c
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-02 05:26:54 +0600

    chefs.json added

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 5000;
 
 
-// const chefs = require('./data/chefs.json');
+const chefs = require('./data/chefs.json');
 
 app.get('/', (req, res) => {
     res.send('The Indic Cuisine Server is running!')
 });
 
-
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
 
 app.listen(port, ()=>{
