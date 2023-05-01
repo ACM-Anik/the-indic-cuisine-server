4656d1c9dc91a46057f44767c60966c5a856af4d
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-02 05:17:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+// const chefs = require('./data/chefs.json');
+
+app.get('/', (req, res) => {
+    res.send('The Indic Cuisine Server is running!')
+});
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+});
